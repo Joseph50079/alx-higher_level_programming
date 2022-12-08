4a790b7847a5003fb9bf33b33642f0240623c929
Joseph50079 <josephogbah2019@gmail.com>
2022-12-07 21:41:31 -0800

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,7 @@
 # checks if c is a lower upper case
 
 def uppercase(str):
+    str1 = ''
     for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             a = ord(str[i])
