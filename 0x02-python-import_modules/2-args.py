628458c6f728504d294d2e55c2090c130458c175
Joseph50079 <josephogbah2019@gmail.com>
2022-12-10 17:29:17 -0800

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     if (len(argv)) == 1:
         print("0 argument.")
     else:
-        if len(argv) == 1:
+        if len(argv) == 2:
             print("{} argument:".format(len(argv) - 1))
         else:
             print("{} arguments:".format(len(argv) - 1))
