b51f8efd6bb3954df44177c68f622987f60a43be
Joseph50079 <josephogbah2019@gmail.com>
2022-12-10 18:51:54 -0800

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,9 +6,11 @@ This script output addtion of all arguments given as integer
 if __name__ == "__main__":
     from sys import argv
 
+    num = 0
     if len(argv) != 1:
-        num = 0
         for i in list(argv[1:]):
             i = int(i)
-            num  += i
+            num += i
+        print("{:d}".format(num))
+    else:
         print("{:d}".format(num))
