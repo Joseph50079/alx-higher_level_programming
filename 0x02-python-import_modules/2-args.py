b55354e60e88f35c6808a794a28e32fc3ec91009
Joseph50079 <josephogbah2019@gmail.com>
2022-12-10 17:26:59 -0800

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+'''
+this function prints argument from the termina
+else if no argument is given it returns 0 argument
+if 1 argument returns 1 arguments and else multiple arguments and a newline
+'''
 
 from sys import argv
 if __name__ == "__main__":
     if (len(argv)) == 1:
         print("0 argument.")
     else:
-        print("{} argument:".format(len(argv) - 1))
+        if len(argv) == 1:
+            print("{} argument:".format(len(argv) - 1))
+        else:
+            print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
