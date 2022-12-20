b568199f6bd479d99f3ca232cd1fc00201a8225b
Joseph50079 <josephogbah2019@gmail.com>
2022-12-19 19:52:48 -0800

    Exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list_integer(my_list=[], x=0):
+    i, count = 0, 0
+    try:
+        while (i < x)
+        print("{:d}".format(my_list[i]), end="")
+        i += 1
+        count += 1
+    except TypeError:
+        pass
+    print()
+    return count
