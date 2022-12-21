c8506799ef1f7c7ebc61ea3fb8ea6c04a6f2dbf6
Joseph50079 <josephogbah2019@gmail.com>
2022-12-20 20:16:11 -0800

    commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 
-'''Class Square defines square'''
+'''
+Class Square defines square
+'''
 
 
 class Square:
-    '''define the class attribute to private'''
+    '''define the class attribute to private
+    Attribute:
+        size: The size of Square
+    '''
     def __init__(self, size=0):
-        '''initializing and creating private instance attributee'''
+        '''initializing and creating private instance attribute
+        size(int): This size of squares
+        '''
         if isinstance(size, int):
             if size < 0:
                 raise ValueError("size must be integer")
