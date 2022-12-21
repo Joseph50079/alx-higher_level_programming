dd6b63832f93bd7daa9994ff5e681d03fcdae27c
Joseph50079 <josephogbah2019@gmail.com>
2022-12-20 20:01:33 -0800

    error handling

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,12 +7,9 @@ class Square:
     '''define the class attribute to private'''
     def __init__(self, size=0):
         '''initializing and creating private instance attributee'''
-        if not isinstance(size, int):
-            raise TypeError('size must be integer')
-            '''prints if there is a type error'''
-        else:
+        if isinstance(size, int):
+            self.__size = size
             if size < 0:
-                raise ValueError('size must be >= 0')
-                '''prints if there was a value error'''
-            else:
-                self.__size = size
+                raise ValueError("size must be integer")
+        else:
+            raise TypeError("size must be integer")
