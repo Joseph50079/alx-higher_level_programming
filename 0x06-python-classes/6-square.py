985f269184bae09b405991c836c395ab8bc0a892
Joseph50079 <josephogbah2019@gmail.com>
2022-12-22 02:41:08 +0100

    still on it

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -70,3 +70,41 @@ class Square:
         else:
             raise TypeError("size must be an integer")
         return self.__size
+    
+    
+    def __init__(self, size=0, position=(0, 0)):
+        '''initializing and creating private instance attribute
+        Args:
+            size(int): This size of squares
+        '''
+
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        '''This is class method returns area
+        Attribute:
+            Args:
+                self attribute to objects
+            Return:
+                area of Square
+        '''
+        self.__size *= self.__size
+        return self.__size
+
+    def my_print(self):
+        '''my_print prints square using '#'
+        if size is less than zeeero it should print a new line
+        else print square
+        '''
+        if self.__size <= 0:
+            print('')
+        else:
+            for i in range(self.__size):
+                for n in range(self.__size):
+                    print('#', end='')
+                print('')
