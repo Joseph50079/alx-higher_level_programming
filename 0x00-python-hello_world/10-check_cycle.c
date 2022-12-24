fe2c73d669e25e7f269eceaccfa31441aca34eb3
Joseph50079 <josephogbah2019@gmail.com>
2022-12-24 09:17:37 -0800

    Technical interview preparation:

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if linked list has a cycle in it
+ * @list: head pointer of node
+ * Return: 0 if there is no cycle else return 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp = list;
+
+	while (temp != NULL)
+	{
+		if (temp->next == list)
+		{
+			return (1)
+		}
+		else
+			temp = temp->next;
+	}
+
+	return (0);
+}
