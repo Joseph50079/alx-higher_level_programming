91cb642904b3c81264a0ecc97c58358816654d44
Joseph50079 <josephogbah2019@gmail.com>
2023-10-09 22:43:17 +0000

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    n = 0
+    if len(my_list) == 0:
+        return None
+    for i in my_list:
+        if n < i:
+            n = i
+    return n
