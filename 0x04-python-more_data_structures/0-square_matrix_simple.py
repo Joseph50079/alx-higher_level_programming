2a954ae6c7f0b8b8a4be24ecaf2b943a0874a18a
Joseph50079 <josephogbah2019@gmail.com>
2023-10-11 21:26:15 +0000

    matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    square = []
+    for i in range(matrix) - 1:
+        a = list(map((lambda x: x * x) , d[i]))
+        square.append(a)
+    return square
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = roman_string
+    t = 0
+    res = 0
+    prev = 0
+    if len(num) == 0 or num == None or not isinstance(num, str):
+        return 0
+    for n in reversed(num):
+        t = dic[n]
+        if prev > t:
+            res -= t
+        else:
+            res += t
+        prev = t
+    return res
