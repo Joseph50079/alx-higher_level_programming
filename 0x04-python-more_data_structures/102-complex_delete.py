1f13535e90bc27ed2b73132e0ba2bb673e9f5133
Joseph50079 <josephogbah2019@gmail.com>
2023-10-12 01:57:32 +0000

    del

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    dic = a_dictionary
+    if not value or None:
+        return dic
+    for i in dic:
+        if dic[i] == value:
+            del dic[i]
+    return dic
