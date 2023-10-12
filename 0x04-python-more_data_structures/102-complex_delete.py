522d09a00d92828e4bac1ed261072cae63373918
Joseph50079 <josephogbah2019@gmail.com>
2023-10-12 02:27:02 +0000

    matrix

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,9 +2,12 @@
 
 def complex_delete(a_dictionary, value):
     dic = a_dictionary
+    l = []
     if not value or None:
         return dic
     for i in dic:
-        if dic[i] == value:
-            del dic[i]
+        if value == dic[i]:
+            l.append(i)
+    for key in l:
+        del dic[key]
     return dic
