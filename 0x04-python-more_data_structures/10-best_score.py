329498dd920188125550b88c09e04f61b526524a
Joseph50079 <josephogbah2019@gmail.com>
2023-10-12 01:10:49 +0000

    score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    n = 0
+    if not a_dictionary.values() or a_dictionary is None:
+        return None
+    for i in a_dictionary:
+        if a_dictionary[i] > n:
+            n = a_dictionary[i]
+            key = i
+    return key
