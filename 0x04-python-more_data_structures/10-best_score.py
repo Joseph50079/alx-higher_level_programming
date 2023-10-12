f366686cf5c4234de3518eb4a6d236ebe227b1da
Joseph50079 <josephogbah2019@gmail.com>
2023-10-12 01:17:25 +0000

    score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 
 def best_score(a_dictionary):
     n = 0
+    if not a_dictionary:
+        return None
     if not a_dictionary.values() or a_dictionary is None:
         return None
     for i in a_dictionary:
