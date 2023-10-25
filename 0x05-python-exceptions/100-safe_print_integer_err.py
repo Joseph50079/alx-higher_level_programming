5dd35ae81c733d38bad24d0209b727233c8ba533
Joseph50079 <josephogbah2019@gmail.com>
2023-10-25 11:23:13 +0000

    err write

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as ex:
-        stderr.write("Exception: {}".format(ex))
+        stderr.write("Exception: {}\n".format(ex))
         return False
