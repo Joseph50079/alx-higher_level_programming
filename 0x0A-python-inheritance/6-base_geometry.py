5154dc1de6fb40d9cdefe7bf2767de2386ac06bf
Joseph50079 <josephogbah2019@gmail.com>
2023-11-16 23:03:04 +0000

    base geo

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Module 5-base_geometry.py"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """ area returns area value"""
+       raise Exception("area() is not implemented")
