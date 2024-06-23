3a43bbab0229bf630354e85b21c6005014a6a462
Joseph50079 <josephogbah2019@gmail.com>
2024-06-23 11:17:51 +0100

    2-argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,10 @@
 
 const { argv } = require('node:process');
 
-if (argv.length > 2) {
+if (argv.length === 3) {
   console.log('Argument found');
+} else if (argv.length > 3) {
+  console.log('Arguments found');
 } else {
-  console.log('No arguments');
+  console.log('No argument');
 }
