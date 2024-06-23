19b2f363fb04efd95b17e746803c75777c7e7fdb
Joseph50079 <josephogbah2019@gmail.com>
2024-06-23 11:25:19 +0100

    3-argument in js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -5,9 +5,5 @@ const { argv } = require('node:process');
 if (!argv[2]) {
   console.log('No argument');
 } else {
-  let i = 2;
-  while (argv[i]) {
-    console.log(argv[i]);
-    i++;
-  }
+  console.log(argv[2]);
 }
