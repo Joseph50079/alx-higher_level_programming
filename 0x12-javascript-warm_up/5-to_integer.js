be3d14cd98b4019bdc76aed9da1d6a157433ec5d
Joseph50079 <josephogbah2019@gmail.com>
2024-06-23 11:48:05 +0100

    5-to intger

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const { argv } = require('process');
 if (isNaN(argv[2])) {
   console.log('Not a number');
 } else {
-  console.log(+argv[2]);
+  console.log('My number: %d', parseInt(+argv[2]));
 }
