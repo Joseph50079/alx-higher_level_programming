98d96a5e7c02edfa87bc20f17fc3b736da0e47e8
Joseph50079 <josephogbah2019@gmail.com>
2024-07-06 14:20:24 +0000

    sub

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays allowed methods
-curl -Is -X OPTIONS $1 | grep 'Allow:' | awk '{ $1=""; print $0 }'
+curl -Is -X OPTIONS $1 | grep 'Allow:' | awk '{ $1=""; sub(/^ /, ""); print $0 }'
