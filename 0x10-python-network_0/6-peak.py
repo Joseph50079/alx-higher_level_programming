a3e9f897bf239be45f47c75d4104c2e9da16fc3e
Joseph50079 <josephogbah2019@gmail.com>
2024-07-07 18:32:55 +0100

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,6 +11,8 @@ def find_peak(list_of_integers):
     """
     if len(list_of_integers) == 0:
         return None
+    elif len(list_of_integers) == 1:
+        return list_of\_integers[0]
 
     def binary_search_peak(low, high):
         if low == high:
