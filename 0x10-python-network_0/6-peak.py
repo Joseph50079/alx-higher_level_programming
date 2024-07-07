5ddf2543c78da7248690d53c71b5d2aac612460f
Joseph50079 <josephogbah2019@gmail.com>
2024-07-07 18:03:28 +0100

    done

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""find the Peak"""
+
+
+def find_peak(list_of_integers):
+    if len(list_of_integers) == 0:
+        return None
+    def binary_search_peak(low, high):
+        if low == high:
+            return low
+
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            return binary_search_peak(mid + 1, high)
+        else:
+            return binary_search_peak(low, mid)
+
+    return list_of_integers[binary_search_peak(0, len(list_of_integers) - 1)]
