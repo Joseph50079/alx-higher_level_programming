752e9cc2df1676c9229dea8ffc2a5687a749eed4
Joseph50079 <josephogbah2019@gmail.com>
2024-07-21 00:52:52 +0100

    sql

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""This module is for ORM sqlalchemy use"""
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+
+Base = declarative_base(metadata=mymetadata)
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
